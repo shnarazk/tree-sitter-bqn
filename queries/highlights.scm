5f4db51be64720237be6397bb379a856ab03ce3f
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-19 01:23:52 +0900

    modified:   queries/highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,21 +1,27 @@
 (comment) @comment
+(number) @constant
 
 "⟨" @punctuation.bracket
 ")" @punctuation.bracket
 "[" @punctuation.bracket
 "]" @punctuation.bracket
 ;"⁼" @punctuation.sep
-"⊸" @operator
-"○" @operator
-"´" @operator
-"˘" @operator
-"⇐" @operator
+; "⊸" @operator
+; "○" @operator
+; "´" @operator
+; "˘" @operator
+; "⇐" @operator
 
 "⋄" @punctuation.sep
 "," @punctuation.sep
 "‿" @punctuation.sep
-; "⍟" @punctuation.bracket
-; "⇐" @keyword
-(symbol__m) @type
-(symbol__c_) @keyword
-(symbol_Fl) @function
+; ; "⍟" @punctuation.bracket
+
+"⇐" @keyword
+
+; (symbol__m) @keyword
+; (symbol__ml) @constructor
+; (symbol__cl_) @keyword
+(symbol_F) @function
+(symbol_Fl) @keyword
+
