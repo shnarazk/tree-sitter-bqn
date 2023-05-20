56884c8ce5c024c2ac6c5dd7db42559760e1ab15
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-20 10:24:27 +0900

    modified:   queries/highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -7,22 +7,18 @@
 (ASGN) @tag
 (system_value) @keyword
 (symbol_sl) @constant.builtin
-(symbol_s) @constant
+; (symbol_s) @constant
 (symbol_Fl) @function.builtin
-(symbol_F) @function
-; (symbol__ml) @function.builtin
-(symbol__m) @keyword.directive
-(symbol__cl_) @special
-(symbol__c_) @special
+; (symbol_F) @function
+(symbol__ml) @type.builtin
+(symbol__m) @type
+(symbol__cl_) @function.special
+(symbol__c_) @function.special
 (comment) @comment
 (character) @constant
 (number) @constant
 (string) @string
 
-"⟨" @punctuation.bracket
-")" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"⋄" @punctuation.delimiter
-"," @punctuation.delimiter
-"‿" @punctuation.delimiter
+"‿" @special
+"⟨" @special
+"⟩" @special
