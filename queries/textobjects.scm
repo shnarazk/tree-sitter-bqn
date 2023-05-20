d0e2ac88b059f557b47f5b4752424088501fd598
Narazaki, Shuji <shujinarazaki@protonmail.com>
2023-05-20 11:52:42 +0900

    modified:   queries/textobjects.scm

--- queries/textobjects.scm
+++ queries/textobjects.scm
@@ -1,3 +1,9 @@
 (comment) @comment.inside
 (comment)+ @comment.around
+(atom) @atom.around
+(Func) @Func.around
+(SubExpr) @EXPR.around
+(FuncExpr) @EXPR.around
+(EXPR) @EXPR.around
+(STMT) @STMT.around
 
